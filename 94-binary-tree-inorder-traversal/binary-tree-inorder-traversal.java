ae65ef324c568567b1c1f64083577ee2fda674d1
Sourish Keshav <98088108+sourish02keshav@users.noreply.github.com>
2026-07-26 15:06:28 +0530

    Time: 0 ms (100.00%) | Memory: 43.2 MB (52.22%) - LeetSync

--- 94-binary-tree-inorder-traversal/binary-tree-inorder-traversal.java
+++ 94-binary-tree-inorder-traversal/binary-tree-inorder-traversal.java
@@ -16,30 +16,59 @@
 class Solution {
     public List<Integer> inorderTraversal(TreeNode root) {
         List<Integer> ans = new ArrayList<>();
-        if(root == null)
+        TreeNode curr = root;
+        while(curr != null)
         {
-            return ans;
-        }
-        Stack<TreeNode> st = new Stack<>();
-        TreeNode node = root;
-        while(true)
-        {
-            if(node != null)
+            if(curr.left == null)
             {
-                st.push(node);
-                node = node.left;
+                ans.add(curr.val);
+                curr = curr.right;
             }
             else
             {
-                if(st.isEmpty())
+                TreeNode prev = curr.left;
+                while(prev.right != null && prev.right != curr)
                 {
-                    break;
+                    prev = prev.right;
                 }
-                node = st.pop();
-                ans.add(node.val);
-                node = node.right;
+                if(prev.right == null)
+                {
+                    prev.right = curr;
+                    curr = curr.left;
+                }
+                else
+                {
+                    prev.right = null;
+                    ans.add(curr.val);
+                    curr = curr.right;
+                }
             }
-        }
+        }        
+        // if(root == null)
+        // {
+        //     return ans;
+        // }
+        // Stack<TreeNode> st = new Stack<>();
+        // TreeNode node = root;
+        // while(true)
+        // {
+        //     if(node != null)
+        //     {
+        //         st.push(node);
+        //         node = node.left;
+        //     }
+        //     else
+        //     {
+        //         if(st.isEmpty())
+        //         {
+        //             break;
+        //         }
+        //         node = st.pop();
+        //         ans.add(node.val);
+        //         node = node.right;
+        //     }
+        // }
+
         //helper(ans,root);
         return ans;
     }
