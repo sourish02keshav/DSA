0c5aa354f157b6c6ffd62713edabec2a7020bca4
Sourish Keshav <98088108+sourish02keshav@users.noreply.github.com>
2026-08-09 23:10:50 +0530

    Time: 1 ms (100.00%) | Memory: 46.6 MB (50.04%) - LeetSync

---
+++ 99-recover-binary-search-tree/recover-binary-search-tree.java
@@ -0,0 +1,66 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+
+    TreeNode first,middle,last,prev;
+
+    public void inorder(TreeNode root)
+    {
+        if(root == null)
+        {
+            return;
+        }
+        inorder(root.left);
+        if(prev != null && root.val < prev.val)
+        {
+            if(first == null)
+            {
+                first = prev;
+                middle = root;
+            }
+            else
+            {
+                last = root;
+            }
+        }
+        prev = root;
+        inorder(root.right);
+    }
+
+    public void swap(TreeNode node1,TreeNode node2)
+    {
+        int temp = node1.val;
+        node1.val = node2.val;
+        node2.val = temp;
+    }
+
+    public void recoverTree(TreeNode root) {
+        if(root == null)
+        {
+            return;
+        }
+        inorder(root);
+        if(first != null && last != null)
+        {
+            swap(first,last);
+        }
+        else if(first != null && middle != null)
+        {
+            swap(first,middle);
+        }
+    }
+    
+}
