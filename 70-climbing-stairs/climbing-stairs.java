921af81e3fe9b15aa3c7f694f99a23e5cb9a16ae
Sourish Keshav <98088108+sourish02keshav@users.noreply.github.com>
2026-08-11 21:28:41 +0530

    Time: 0 ms (100.00%) | Memory: 42 MB (55.19%) - LeetSync

--- 70-climbing-stairs/climbing-stairs.java
+++ 70-climbing-stairs/climbing-stairs.java
@@ -9,15 +9,34 @@ class Solution {
         // }
         // return climbStairs(n - 1) + climbStairs(n - 2);
 
-        // Tabulation
+        // Memoization
 
         int[] dp = new int[n + 1];
-        dp[0] = 1;
-        dp[1] = 1;
-        for(int i = 2;i <= n;i++)
+        Arrays.fill(dp,-1);
+        return climbStairsHelper(n,dp);
+
+        // Tabulation
+
+        // int[] dp = new int[n + 1];
+        // dp[0] = 1;
+        // dp[1] = 1;
+        // for(int i = 2;i <= n;i++)
+        // {
+        //     dp[i] = dp[i - 1] + dp[i - 2];
+        // }
+        // return dp[n];
+    }
+
+    public int climbStairsHelper(int n,int[] dp)
+    {
+        if(n <= 1)
         {
-            dp[i] = dp[i - 1] + dp[i - 2];
+            return 1;
         }
-        return dp[n];
+        if(dp[n] != -1)
+        {
+            return dp[n];
+        }
+        return dp[n] = climbStairsHelper(n - 1,dp) + climbStairsHelper(n - 2,dp);
     }
 }
