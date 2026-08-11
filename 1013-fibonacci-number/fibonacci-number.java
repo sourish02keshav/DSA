78e1504be7b00b3f6be787592a708953457e37fe
Sourish Keshav <98088108+sourish02keshav@users.noreply.github.com>
2026-08-11 20:36:49 +0530

    Time: 0 ms (100.00%) | Memory: 42.1 MB (33.82%) - LeetSync

--- 1013-fibonacci-number/fibonacci-number.java
+++ 1013-fibonacci-number/fibonacci-number.java
@@ -10,9 +10,26 @@ class Solution {
 
         // Memoization
 
+        // int[] dp = new int[n + 1];
+        // Arrays.fill(dp,-1);
+        // return fibHelper(n,dp);
+
+        // Tabulation
+
+        if(n == 0)
+        {
+            return n;
+        }
         int[] dp = new int[n + 1];
-        Arrays.fill(dp,-1);
-        return fibHelper(n,dp);
+        dp[0] = 0;
+        dp[1] = 1;
+
+        for(int i = 2;i <= n;i++)
+        {
+            dp[i] = dp[i - 1] + dp[i - 2];
+        }
+
+        return dp[n];
     }
 
     public int fibHelper(int n,int[] dp)
