ae5c74ab3dbfbdfa005a4bfe8a00d9efe0510a1d
Sourish Keshav <98088108+sourish02keshav@users.noreply.github.com>
2026-08-16 23:00:44 +0530

    Time: 0 ms (100.00%) | Memory: 42.5 MB (90.23%) - LeetSync

---
+++ 213-house-robber-ii/house-robber-ii.java
@@ -0,0 +1,48 @@
+class Solution {
+
+    int robHelper(int[] arr)
+    {
+        int n = arr.length;
+        int prev = arr[0];
+        int prev2 = 0;
+        for(int i = 1;i < n;i++)
+        {
+            int take = arr[i];
+            if(i > 1)
+            {
+                take += prev2;
+            }
+            int notTake = prev;
+            int currSum = Math.max(take,notTake);
+            prev2 = prev;
+            prev = currSum;
+        }
+        return prev;
+    }
+
+    public int rob(int[] nums) {
+        int n = nums.length;
+        if(n == 0)
+        {
+            return 0;
+        }
+        if(n == 1)
+        {
+            return nums[0];
+        }
+        int[] temp1 = new int[n - 1];
+        int[] temp2 = new int[n - 1];
+        for(int i = 0;i < n;i++)
+        {
+            if(i != 0)
+            {
+                temp1[i - 1] = nums[i];
+            }
+            if(i != n - 1)
+            {
+                temp2[i] = nums[i];
+            }
+        }
+        return Math.max(robHelper(temp1),robHelper(temp2));
+    }
+}
