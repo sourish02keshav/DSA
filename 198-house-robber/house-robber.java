d83f19961a528804578cc1e7875b2e2c72614bbe
Sourish Keshav <98088108+sourish02keshav@users.noreply.github.com>
2026-08-16 22:13:21 +0530

    Time: 0 ms (100.00%) | Memory: 42.6 MB (75.71%) - LeetSync

---
+++ 198-house-robber/house-robber.java
@@ -0,0 +1,49 @@
+class Solution {
+    public int rob(int[] nums) {
+
+        // Memoization
+
+        int n = nums.length;
+        int[] dp = new int[n];
+        Arrays.fill(dp,-1);
+        return robMemoization(n - 1,nums,dp);
+
+        // Recursion
+        // return robHelper(nums.length - 1,nums);
+    }
+
+    public int robMemoization(int n,int[] arr,int[] dp)
+    {
+        if(n == 0)
+        {
+            return arr[n];
+        }
+        if(n < 0)
+        {
+            return 0;
+        }
+        if(dp[n] != -1)
+        {
+            return dp[n];
+        }
+        int take = arr[n] + robMemoization(n - 2,arr,dp);
+        int notTake = robMemoization(n - 1,arr,dp);
+        return dp[n] = Math.max(take,notTake);
+    }
+
+    public int robHelper(int n,int[] arr)
+    {
+        if(n == 0)
+        {
+            return arr[0];
+        }
+        if(n < 0)
+        {
+            return 0;
+        }
+
+        int take = arr[n] + robHelper(n - 2,arr);
+        int notTake = robHelper(n - 1,arr);
+        return Math.max(take,notTake);
+    }
+}
