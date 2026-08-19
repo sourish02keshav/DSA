b2bedd6b2f3313c623e934285005e6c5be94f142
Sourish Keshav <98088108+sourish02keshav@users.noreply.github.com>
2026-08-19 23:46:46 +0530

    Time: 0 ms (100.00%) | Memory: 43.4 MB (81.62%) - LeetSync

--- 63-unique-paths-ii/unique-paths-ii.java
+++ 63-unique-paths-ii/unique-paths-ii.java
@@ -4,16 +4,48 @@ class Solution {
         int m = obstacleGrid.length;
         int n = obstacleGrid[0].length;
 
+        // Tabulation
+
         int[][] dp = new int[m][n];
         for(int i = 0;i < m;i++)
         {
             for(int j = 0;j < n;j++)
             {
-                dp[i][j] = -1;
+                if(obstacleGrid[i][j] == 1)
+                {
+                    continue;
+                }
+                if(i == 0 && j == 0)
+                {
+                    dp[i][j] = 1;
+                }
+                else
+                {
+                    if(i > 0)
+                    {
+                        dp[i][j] += dp[i - 1][j];
+                    }
+                    if(j > 0)
+                    {
+                        dp[i][j] += dp[i][j - 1];
+                    }
+                }
             }
         }
+        return dp[m - 1][n - 1];
 
-        return uniquePathsWithObstaclesMemoization(obstacleGrid,m,n,m - 1,n - 1,dp);
+        // Memoization
+
+        // int[][] dp = new int[m][n];
+        // for(int i = 0;i < m;i++)
+        // {
+        //     for(int j = 0;j < n;j++)
+        //     {
+        //         dp[i][j] = -1;
+        //     }
+        // }
+
+        // return uniquePathsWithObstaclesMemoization(obstacleGrid,m,n,m - 1,n - 1,dp);
 
         // Recursion
         //return uniquePathsWithObstaclesHelper(obstacleGrid,m,n,m - 1,n - 1);
