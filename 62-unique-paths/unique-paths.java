1bd49f582a1f77e409027bb1f9b614708fb40f2e
Sourish Keshav <98088108+sourish02keshav@users.noreply.github.com>
2026-08-19 23:13:20 +0530

    Time: 0 ms (100.00%) | Memory: 42 MB (82.97%) - LeetSync

--- 62-unique-paths/unique-paths.java
+++ 62-unique-paths/unique-paths.java
@@ -1,73 +1,19 @@
 class Solution {
     public int uniquePaths(int m, int n) {
 
-        // Space Optimization
+        // Memoization
 
-        int[] prev = new int[n];
-        for(int i = 0;i < m ;i++)
+        int[][] dp = new int[m][n];
+        for(int i = 0;i < m;i++)
         {
-            int[] temp = new int[n];
             for(int j = 0;j < n;j++)
             {
-                if(i == 0 && j == 0)
-                {
-                    temp[j] = 1;
-                }
-                else
-                {
-                    temp[j] += prev[j];
-                    if(j > 0)
-                    {
-                        temp[j] += temp[j - 1];
-                    }
-                }
+                dp[i][j] = -1;
             }
-            prev = temp;
         }
-
-        return prev[n - 1];
+        return uniquePathsMemoization(m,n,m - 1,n - 1,dp);
         
-        // Tabulation - TC - O(m * n) and SC - O(m * n)
-
-        // int[][] dp = new int[m][n];
-        // dp[0][0] = 1;
-        // for(int i = 0;i < m;i++)
-        // {
-        //     for(int j = 0;j < n;j++)
-        //     {
-        //         if(i == 0 && j == 0)
-        //         {
-        //             dp[i][j] = 1;
-        //         }
-        //         else
-        //         {
-        //             if(i > 0)
-        //             {
-        //                 dp[i][j] += dp[i - 1][j];
-        //             }
-        //             if(j > 0)
-        //             {
-        //                 dp[i][j] += dp[i][j - 1];
-        //             }
-        //         }
-        //     }
-        // }
-        // return dp[m - 1][n - 1];
-
-        // Memoization - TC - O(m * n) and SC - O((m - 1) + (n - 1)) + O(m * n)
-
-        // int[][] dp = new int[m][n];
-        // for(int i = 0;i < m;i++)
-        // {
-        //     for(int j = 0;j < n;j++)
-        //     {
-        //         dp[i][j] = -1;
-        //     }
-        // }
-
-        // return uniquePathsMemoization(m,n,m - 1,n - 1,dp);
-
-        // Recursion - TC - O(2 ^ (m * n)) and SC - O((m - 1) + (n - 1))
+        // Recursion - TC - O(2 ^ (m * n)) and SC - O(m * n)
         //return uniquePathsHelper(m,n,m - 1,n - 1);
     }
 
