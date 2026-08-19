6520513dd713892f13e1486eb653646852bf5625
Sourish Keshav <98088108+sourish02keshav@users.noreply.github.com>
2026-08-19 23:44:24 +0530

    Time: 0 ms (100.00%) | Memory: 43.6 MB (46.21%) - LeetSync

--- 63-unique-paths-ii/unique-paths-ii.java
+++ 63-unique-paths-ii/unique-paths-ii.java
@@ -3,50 +3,23 @@ class Solution {
 
         int m = obstacleGrid.length;
         int n = obstacleGrid[0].length;
-        int[] prev = new int[n];
+
+        int[][] dp = new int[m][n];
         for(int i = 0;i < m;i++)
         {
-            int[] temp = new int[n];
             for(int j = 0;j < n;j++)
             {
-                if(obstacleGrid[i][j] == 1)
-                {
-                    continue;
-                }
-                if(i == 0 && j == 0)
-                {
-                    temp[j] = 1;
-                }
-                else
-                {
-                    if(i > 0)
-                    {
-                        temp[j] += prev[j];
-                    }
-                    if(j > 0)
-                    {
-                        temp[j] += temp[j - 1];
-                    }
-                }
+                dp[i][j] = -1;
             }
-            prev = temp;
         }
-        return prev[n - 1];
 
-        // int m = obstacleGrid.length;
-        // int n = obstacleGrid[0].length;
-        // int[][] dp = new int[m][n];
-        // for(int i = 0;i < m;i++)
-        // {
-        //     for(int j = 0;j < n;j++)
-        //     {
-        //         dp[i][j] = -1;
-        //     }
-        // }
-        // return uniquePathsWithObstaclesHelper(obstacleGrid,m,n,m - 1,n - 1,dp);
+        return uniquePathsWithObstaclesMemoization(obstacleGrid,m,n,m - 1,n - 1,dp);
+
+        // Recursion
+        //return uniquePathsWithObstaclesHelper(obstacleGrid,m,n,m - 1,n - 1);
     }
 
-    public int uniquePathsWithObstaclesHelper(int[][] grid,int m,int n,int i,int j,int[][] dp)
+    public int uniquePathsWithObstaclesMemoization(int[][] grid,int m,int n,int i,int j,int[][] dp)
     {
         if(i < 0 || j < 0 || grid[i][j] == 1)
         {
@@ -60,7 +33,21 @@ class Solution {
         {
             return dp[i][j];
         }
-        return dp[i][j] = uniquePathsWithObstaclesHelper(grid,m,n,i - 1,j,dp) + 
-        uniquePathsWithObstaclesHelper(grid,m,n,i,j - 1,dp);
+        return dp[i][j] = uniquePathsWithObstaclesMemoization(grid,m,n,i - 1,j,dp) + 
+        uniquePathsWithObstaclesMemoization(grid,m,n,i,j - 1,dp);
+    }
+
+    public int uniquePathsWithObstaclesHelper(int[][] grid,int m,int n,int i,int j)
+    {
+        if(i < 0 || j < 0 || grid[i][j] == 1)
+        {
+            return 0; 
+        }
+        if(i == 0 && j == 0)
+        {
+            return 1;
+        }
+        return uniquePathsWithObstaclesHelper(grid,m,n,i - 1,j) +
+        uniquePathsWithObstaclesHelper(grid,m,n,i,j - 1);
     }
 }
