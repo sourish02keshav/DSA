87dfa9538cb715596035bf200cf55f7fd18c131b
Sourish Keshav <98088108+sourish02keshav@users.noreply.github.com>
2026-08-19 23:37:06 +0530

    Time: 0 ms (100.00%) | Memory: 43.6 MB (27.18%) - LeetSync

--- 63-unique-paths-ii/unique-paths-ii.java
+++ 63-unique-paths-ii/unique-paths-ii.java
@@ -1,5 +1,6 @@
 class Solution {
     public int uniquePathsWithObstacles(int[][] obstacleGrid) {
+
         int m = obstacleGrid.length;
         int n = obstacleGrid[0].length;
         int[][] dp = new int[m][n];
@@ -7,10 +8,40 @@ class Solution {
         {
             for(int j = 0;j < n;j++)
             {
-                dp[i][j] = -1;
+                if(obstacleGrid[i][j] == 1)
+                {
+                    continue;
+                }
+                if(i == 0 && j == 0)
+                {
+                    dp[i][j] = 1;
+                }
+                else
+                {
+                    if(i > 0)
+                    {
+                        dp[i][j] += dp[i - 1][j];
+                    }
+                    if(j > 0)
+                    {
+                        dp[i][j] += dp[i][j - 1];
+                    }
+                }
             }
         }
-        return uniquePathsWithObstaclesHelper(obstacleGrid,m,n,m - 1,n - 1,dp);
+        return dp[m - 1][n - 1];
+
+        // int m = obstacleGrid.length;
+        // int n = obstacleGrid[0].length;
+        // int[][] dp = new int[m][n];
+        // for(int i = 0;i < m;i++)
+        // {
+        //     for(int j = 0;j < n;j++)
+        //     {
+        //         dp[i][j] = -1;
+        //     }
+        // }
+        // return uniquePathsWithObstaclesHelper(obstacleGrid,m,n,m - 1,n - 1,dp);
     }
 
     public int uniquePathsWithObstaclesHelper(int[][] grid,int m,int n,int i,int j,int[][] dp)
