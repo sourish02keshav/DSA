5cb8be1e843f7c1797203b85ecddc903ae112bdd
Sourish Keshav <98088108+sourish02keshav@users.noreply.github.com>
2026-08-21 20:06:21 +0530

    Time: 5 ms (24.54%) | Memory: 50.1 MB (85.64%) - LeetSync

--- 64-minimum-path-sum/minimum-path-sum.java
+++ 64-minimum-path-sum/minimum-path-sum.java
@@ -3,14 +3,18 @@ class Solution {
         int m = grid.length;
         int n = grid[0].length;
 
-        int[][] dp = new int[m][n];
+        // Space Optimization
+
+        int[] prev = new int[n];
+
         for(int i = 0;i < m;i++)
         {
+            int[] temp = new int[n];
             for(int j = 0;j < n;j++)
             {
                 if(i == 0 && j == 0)
                 {
-                    dp[i][j] = grid[i][j];
+                    temp[j] = grid[i][j];
                 }
                 else
                 {
@@ -18,25 +22,63 @@ class Solution {
                     int right = grid[i][j];
                     if(i > 0)
                     {
-                        down += dp[i - 1][j];
+                        down += prev[j];
                     }
                     else
                     {
-                        down += 1000000;
+                        down += 100000;
                     }
                     if(j > 0)
                     {
-                        right += dp[i][j - 1];
+                        right += temp[j - 1];
                     }
                     else
                     {
-                        right += 1000000;
+                        right += 100000;
                     }
-                    dp[i][j] = Math.min(down,right);
+                    temp[j] = Math.min(down,right);
                 }
             }
+            prev = temp;
         }
-        return dp[m - 1][n - 1];
+        return prev[n - 1];
+
+        // Tabulation
+
+        // int[][] dp = new int[m][n];
+        // for(int i = 0;i < m;i++)
+        // {
+        //     for(int j = 0;j < n;j++)
+        //     {
+        //         if(i == 0 && j == 0)
+        //         {
+        //             dp[i][j] = grid[i][j];
+        //         }
+        //         else
+        //         {
+        //             int down = grid[i][j];
+        //             int right = grid[i][j];
+        //             if(i > 0)
+        //             {
+        //                 down += dp[i - 1][j];
+        //             }
+        //             else
+        //             {
+        //                 down += 1000000;
+        //             }
+        //             if(j > 0)
+        //             {
+        //                 right += dp[i][j - 1];
+        //             }
+        //             else
+        //             {
+        //                 right += 1000000;
+        //             }
+        //             dp[i][j] = Math.min(down,right);
+        //         }
+        //     }
+        // }
+        // return dp[m - 1][n - 1];
 
         // Memoization
         // int[][] dp = new int[m][n];
