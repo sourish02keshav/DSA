a47338ee6b5ee56689522e419057e4a40b6caf7f
Sourish Keshav <98088108+sourish02keshav@users.noreply.github.com>
2026-08-23 19:57:04 +0530

    Time: 3 ms (79.31%) | Memory: 45.7 MB (40.50%) - LeetSync

--- 120-triangle/triangle.java
+++ 120-triangle/triangle.java
@@ -1,105 +1,62 @@
 class Solution {
     public int minimumTotal(List<List<Integer>> triangle) {
         int m = triangle.size();
-        List<List<Integer>> dp = new ArrayList<>();
+        int[][] dp = new int[m][m];
 
-        // Space Optimization
-
-        List<Integer> next = new ArrayList<>();
-        for(int i = 0;i < m;i++)
+        for(int j = 0;j < m;j++)
         {
-            next.add(triangle.get(m - 1).get(i));
+            dp[m - 1][j] = triangle.get(m - 1).get(j);
         }
+
         for(int i = m - 2;i >= 0;i--)
         {
-            List<Integer> temp = new ArrayList<>();
-            for(int j = 0;j < triangle.get(i).size();j++)
+            for(int j = i;j >= 0;j--)
             {
-                temp.add(0);
+                dp[i][j] = triangle.get(i).get(j) + Math.min(dp[i + 1][j],dp[i + 1][j + 1]);
             }
-            for(int j = triangle.get(i).size() - 1;j >= 0;j--)
-            {
-                int left = triangle.get(i).get(j) + next.get(j);
-                int right = triangle.get(i).get(j) + next.get(j + 1);
-                temp.set(j,Math.min(left,right));
-            }
-            next = temp;
         }
 
-        return next.get(0);
-
-
-        // Tabulation
-
-        // for(int i = 0;i < triangle.size();i++)
-        // {
-        //     dp.add(new ArrayList<>());
-        //     for(int j = 0;j < triangle.get(i).size();j++)
-        //     {
-        //         dp.get(i).add(0);
-        //     }
-        // }
-
-        // for(int i = m - 1;i >= 0;i--)
-        // {
-        //     for(int j = triangle.get(i).size() - 1;j >= 0;j--)
-        //     {
-        //         if(i == m - 1)
-        //         {
-        //             dp.get(i).set(j,triangle.get(i).get(j));
-        //         }
-        //         else
-        //         {
-        //             int left = triangle.get(i).get(j) + dp.get(i + 1).get(j);
-        //             int right = triangle.get(i).get(j) + dp.get(i + 1).get(j + 1);
-        //             dp.get(i).set(j,Math.min(left,right));
-        //         }
-        //     }
-        // }
-        // return dp.get(0).get(0);
+        return dp[0][0];
 
         // Memoization
 
         // for(int i = 0;i < m;i++)
         // {
-        //     dp.add(new ArrayList<>());
-        //     for(int j = 0;j < triangle.get(i).size();j++)
+        //     for(int j = 0;j < m;j++)
         //     {
-        //         dp.get(i).add(-1);
+        //         dp[i][j] = -1;
         //     }
         // }
 
         // return minTotalMemoization(triangle,dp,m,0,0);
 
         // Recursion
-        // return minimumTotalHelper(triangle,m,0,0);
+        // return minTotalHelper(triangle,m,0,0);
     }
 
-    public int minTotalMemoization(List<List<Integer>> list,List<List<Integer>> dp,int m,int i,int j)
+    public int minTotalMemoization(List<List<Integer>> list,int[][] dp,int m,int i,int j)
     {
         if(i == m - 1)
         {
             return list.get(i).get(j);
         }
-        if(dp.get(i).get(j) != -1)
+        if(dp[i][j] != -1)
         {
-            return dp.get(i).get(j);
+            return dp[i][j];
         }
         int left = list.get(i).get(j) +  minTotalMemoization(list,dp,m,i + 1,j);
         int right = list.get(i).get(j) + minTotalMemoization(list,dp,m,i + 1,j + 1);
-        int ans = Math.min(left,right);
-        dp.get(i).set(j,ans);
-        return ans;
+        return dp[i][j] = Math.min(left,right);
     }
 
-    public int minimumTotalHelper(List<List<Integer>> list,int m,int i,int j)
+    public int minTotalHelper(List<List<Integer>> list,int m,int i,int j)
     {
         if(i == m - 1)
         {
             return list.get(i).get(j);
         }
-        int left = list.get(i).get(j) +  minimumTotalHelper(list,m,i + 1,j);
-        int right = list.get(i).get(j) + minimumTotalHelper(list,m,i + 1,j + 1);
+        int left = list.get(i).get(j) + minTotalHelper(list,m,i + 1,j);
+        int right = list.get(i).get(j) + minTotalHelper(list,m,i + 1,j + 1);
         return Math.min(left,right);
     }
 }
