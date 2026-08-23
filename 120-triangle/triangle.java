b2220f5b97314049b760ed4b719aa8f6ce798289
Sourish Keshav <98088108+sourish02keshav@users.noreply.github.com>
2026-08-23 18:45:24 +0530

    Time: 14 ms (7.40%) | Memory: 46.5 MB (18.95%) - LeetSync

---
+++ 120-triangle/triangle.java
@@ -0,0 +1,79 @@
+class Solution {
+    public int minimumTotal(List<List<Integer>> triangle) {
+        int m = triangle.size();
+        List<List<Integer>> dp = new ArrayList<>();
+
+        // Tabulation
+
+        for(int i = 0;i < triangle.size();i++)
+        {
+            dp.add(new ArrayList<>());
+            for(int j = 0;j < triangle.get(i).size();j++)
+            {
+                dp.get(i).add(0);
+            }
+        }
+
+        for(int i = m - 1;i >= 0;i--)
+        {
+            for(int j = triangle.get(i).size() - 1;j >= 0;j--)
+            {
+                if(i == m - 1)
+                {
+                    dp.get(i).set(j,triangle.get(i).get(j));
+                }
+                else
+                {
+                    int left = triangle.get(i).get(j) + dp.get(i + 1).get(j);
+                    int right = triangle.get(i).get(j) + dp.get(i + 1).get(j + 1);
+                    dp.get(i).set(j,Math.min(left,right));
+                }
+            }
+        }
+        return dp.get(0).get(0);
+
+        // Memoization
+
+        // for(int i = 0;i < m;i++)
+        // {
+        //     dp.add(new ArrayList<>());
+        //     for(int j = 0;j < triangle.get(i).size();j++)
+        //     {
+        //         dp.get(i).add(-1);
+        //     }
+        // }
+
+        // return minTotalMemoization(triangle,dp,m,0,0);
+
+        // Recursion
+        // return minimumTotalHelper(triangle,m,0,0);
+    }
+
+    public int minTotalMemoization(List<List<Integer>> list,List<List<Integer>> dp,int m,int i,int j)
+    {
+        if(i == m - 1)
+        {
+            return list.get(i).get(j);
+        }
+        if(dp.get(i).get(j) != -1)
+        {
+            return dp.get(i).get(j);
+        }
+        int left = list.get(i).get(j) +  minTotalMemoization(list,dp,m,i + 1,j);
+        int right = list.get(i).get(j) + minTotalMemoization(list,dp,m,i + 1,j + 1);
+        int ans = Math.min(left,right);
+        dp.get(i).set(j,ans);
+        return ans;
+    }
+
+    public int minimumTotalHelper(List<List<Integer>> list,int m,int i,int j)
+    {
+        if(i == m - 1)
+        {
+            return list.get(i).get(j);
+        }
+        int left = list.get(i).get(j) +  minimumTotalHelper(list,m,i + 1,j);
+        int right = list.get(i).get(j) + minimumTotalHelper(list,m,i + 1,j + 1);
+        return Math.min(left,right);
+    }
+}
