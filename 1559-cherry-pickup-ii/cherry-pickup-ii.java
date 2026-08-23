d57dad2eb7472cadc264c00470466023d9c0c0d8
Sourish Keshav <98088108+sourish02keshav@users.noreply.github.com>
2026-08-24 00:27:28 +0530

    Time: 32 ms (24.70%) | Memory: 46.2 MB (89.27%) - LeetSync

--- 1559-cherry-pickup-ii/cherry-pickup-ii.java
+++ 1559-cherry-pickup-ii/cherry-pickup-ii.java
@@ -2,75 +2,24 @@ class Solution {
     public int cherryPickup(int[][] grid) {
         int m = grid.length;
         int n = grid[0].length;
+
         int[][][] dp = new int[m][n][n];
 
-        for(int j1 = 0;j1 < n;j1++)
+        for(int i = 0;i < m;i++)
         {
-            for(int j2 = 0;j2 < n;j2++)
+            for(int j = 0;j < n;j++)
             {
-                if(j1 == j2)
+                for(int k = 0;k < n;k++)
                 {
-                    dp[m - 1][j1][j2] = grid[m - 1][j1];
-                }
-                else
-                {
-                    dp[m - 1][j1][j2] = grid[m - 1][j1] + grid[m - 1][j2];
+                    dp[i][j][k] = -1;
                 }
             }
         }
 
-        for(int i = m - 2;i >= 0;i--)
-        {
-            for(int j1 = 0;j1 < n;j1++)
-            {
-                for(int j2 = 0;j2 < n;j2++)
-                {
-                    int max = -10000;
-                    for(int dir1 = -1;dir1 <= 1;dir1++)
-                    {
-                        for(int dir2 = -1;dir2 <= 1;dir2++)
-                        {
-                            int value = 0;
-                            if(j1 == j2)
-                            {
-                                value = grid[i][j1];
-                            }
-                            else
-                            {
-                                value = grid[i][j1] + grid[i][j2];
-                            }
-                            if(j1 + dir1 >= 0 && j1 + dir1 < n && j2 + dir2 >= 0 && j2 + dir2 < n)
-                            {
-                                value += dp[i + 1][j1 + dir1][j2 + dir2];
-                            }
-                            else
-                            {
-                                value += -100000;
-                            }
-                            max = Math.max(value,max);
-                        }
-                    }
-                    dp[i][j1][j2] = max;
-                }
-            }
-        }
+        return fMemoi(grid,0,0,n - 1,m,n,dp);
 
-        return dp[0][0][n - 1];
-
-        // for(int i = 0;i < m;i++)
-        // {
-        //     for(int j = 0;j < n;j++)
-        //     {
-        //         for(int k = 0;k < n;k++)
-        //         {
-        //             dp[i][j][k] = -1;
-        //         }
-        //     }
-        // }
-
-        // return fMemoi(grid,0,0,n - 1,m,n,dp);
-
-        //return f(grid,0,0,n - 1,m,n);
+        // Recursion
+        // return f(grid,0,0,n - 1,m,n);
     }
 
     public int fMemoi(int[][] grid,int i,int j1,int j2,int m,int n,int[][][] dp)
@@ -102,11 +51,11 @@ class Solution {
                 int value = 0;
                 if(j1 == j2)
                 {
-                    value = grid[i][j1];
+                    value += grid[i][j1];
                 }
                 else
                 {
-                    value = grid[i][j1] + grid[i][j2];
+                    value += grid[i][j1] + grid[i][j2];
                 }
                 max = Math.max(max,value + fMemoi(grid,i + 1,j1 + dir1,j2 + dir2,m,n,dp));
             }
@@ -139,11 +88,11 @@ class Solution {
                 int value = 0;
                 if(j1 == j2)
                 {
-                    value = grid[i][j1];
+                    value += grid[i][j1];
                 }
                 else
                 {
-                    value = grid[i][j1] + grid[i][j2];
+                    value += grid[i][j1] + grid[i][j2];
                 }
                 max = Math.max(max,value + f(grid,i + 1,j1 + dir1,j2 + dir2,m,n));
             }
