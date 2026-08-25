b7328e8bae607857c7f28ab02d7967821e393bc0
Sourish Keshav <98088108+sourish02keshav@users.noreply.github.com>
2026-08-25 19:37:53 +0530

    Time: 93 ms (23.94%) | Memory: 76 MB (12.93%) - LeetSync

---
+++ 416-partition-equal-subset-sum/partition-equal-subset-sum.java
@@ -0,0 +1,80 @@
+class Solution {
+    public boolean canPartition(int[] nums) {
+        int n = nums.length;
+        int totSum = 0;
+        for(int i = 0;i < n;i++)
+        {
+            totSum += nums[i];
+        }
+        if(totSum % 2 != 0)
+        {
+            return false;
+        }
+        else
+        {
+            return isSubsetSum(n,totSum / 2,nums);
+        }
+    }
+
+    public boolean isSubsetSum(int n,int sum,int[] arr) {
+
+        // Memoization
+        int[][] dp = new int[n][sum + 1];
+        for(int i = 0;i < n;i++)
+        {
+            for(int j = 0;j <= sum;j++)
+            {
+                dp[i][j] = -1;
+            }
+        }
+
+        return fMemoi(n - 1,sum,arr,dp);
+
+        // Recursion
+        // return f(n - 1,sum,arr);
+
+    }
+
+    public boolean fMemoi(int idx,int sum,int[] arr,int[][] dp)
+    {
+        if(sum == 0)
+        {
+            return true;
+        }
+        if(idx == 0)
+        {
+            return arr[idx] == sum;
+        }
+        if(dp[idx][sum] != -1)
+        {
+            return dp[idx][sum] == 1;
+        }
+        boolean notTake = fMemoi(idx - 1,sum,arr,dp);
+        boolean take = false;
+        if(arr[idx] <= sum)
+        {
+            take = fMemoi(idx - 1,sum - arr[idx],arr,dp);
+        }
+        dp[idx][sum] = notTake || take ? 1 : 0;
+        return notTake || take;
+    }
+
+    public boolean f(int idx,int sum,int[] arr)
+    {
+        if(sum == 0)
+        {
+            return true;
+        }
+        if(idx == 0)
+        {
+            return arr[0] == sum;
+        }
+        boolean notTake = f(idx - 1,sum,arr);
+        boolean take = false;
+        if(arr[idx] <= sum)
+        {
+            take = f(idx - 1,sum - arr[idx],arr);
+        }
+        return take || notTake;
+    }
+}
