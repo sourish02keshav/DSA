cf9158c50b6cad8c9012afc4912bd03de67a9c0c
Sourish Keshav <98088108+sourish02keshav@users.noreply.github.com>
2026-09-05 03:07:59 +0530

    Time: 15 ms (60.07%) | Memory: 51.5 MB (55.78%) - LeetSync

--- 455-assign-cookies/assign-cookies.java
+++ 455-assign-cookies/assign-cookies.java
@@ -3,19 +3,20 @@ class Solution {
         Arrays.sort(g);
         Arrays.sort(s);
         int l = 0,r = 0;
-        int m = g.length,n = s.length;
+        int m = g.length;
+        int n = s.length;
         while(r < n)
         {
             if(l >= m)
             {
                 break;
             }
-            else if(g[l] <= s[r])
+            if(g[l] <= s[r])
             {
                 l = l + 1;
             }
             r = r + 1;
-        }
+        }   
         return l;
     }
 }
