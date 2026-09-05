7288e0947a328d1b73d39286261929aa0957ec40
Sourish Keshav <98088108+sourish02keshav@users.noreply.github.com>
2026-09-05 12:30:20 +0530

    Time: 16 ms (70.52%) | Memory: 46.9 MB (35.33%) - LeetSync

--- 322-coin-change/coin-change.java
+++ 322-coin-change/coin-change.java
@@ -2,27 +2,58 @@ class Solution {
     public int coinChange(int[] coins, int amount) {
         int n = coins.length;
         int[][] dp = new int[n][amount + 1];
-        
-        // Memoization
 
-        for(int i = 0;i < n;i++)
+        for(int j = 0;j <= amount;j++)
         {
-            for(int j = 0;j <= amount;j++)
+            if(j % coins[0] == 0)
             {
-                dp[i][j] = -1;
+                dp[0][j] = j / coins[0];
+            }
+            else
+            {
+                dp[0][j] = (int)1e9;
             }
         }
 
-        int ans = fMemoi(n - 1,amount,coins,dp);
+        for(int i = 1;i < n;i++)
+        {
+            for(int j = 0;j <= amount;j++)
+            {
+                int notTake = dp[i - 1][j];
+                int take = (int)1e9;
+                if(coins[i] <= j)
+                {
+                    take = 1 + dp[i][j - coins[i]];
+                }
+                dp[i][j] = Math.min(take,notTake);
+            }
+        }
+        if(dp[n - 1][amount] >= 1e9)
+        {
+            return -1;
+        }
+        return dp[n - 1][amount];
+
+        // Memoization
+
+        // for(int i = 0;i < n;i++)
+        // {
+        //     for(int j = 0;j <= amount;j++)
+        //     {
+        //         dp[i][j] = -1;
+        //     }
+        // }
+
+        // int ans = fMemoi(n - 1,amount,coins,dp);
 
         // Recursion
         // int ans = f(n - 1,coins,amount);
 
-        if(ans >= 1e7)
-        {
-            return -1;
-        }
-        return ans;
+        // if(ans >= 1e7)
+        // {
+        //     return -1;
+        // }
+        // return ans;
     }
 
     public int fMemoi(int idx,int amount,int[] coins,int[][] dp)
