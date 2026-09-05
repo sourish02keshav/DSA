e6d40b050e3f2984636c37ee0398c5766c9dba59
Sourish Keshav <98088108+sourish02keshav@users.noreply.github.com>
2026-09-05 12:37:44 +0530

    Time: 16 ms (70.52%) | Memory: 46.9 MB (30.56%) - LeetSync

--- 322-coin-change/coin-change.java
+++ 322-coin-change/coin-change.java
@@ -3,36 +3,75 @@ class Solution {
         int n = coins.length;
         int[][] dp = new int[n][amount + 1];
 
+        // Space Optimization
+
+        int[] prev = new int[amount + 1];
         for(int j = 0;j <= amount;j++)
         {
             if(j % coins[0] == 0)
             {
-                dp[0][j] = j / coins[0];
+                prev[j] = j / coins[0];
             }
             else
             {
-                dp[0][j] = (int)1e9;
+                prev[j] = Integer.MAX_VALUE;
             }
         }
 
         for(int i = 1;i < n;i++)
         {
+            int[] curr = new int[amount + 1];
             for(int j = 0;j <= amount;j++)
             {
-                int notTake = dp[i - 1][j];
+                int notTake = prev[j];
                 int take = (int)1e9;
                 if(coins[i] <= j)
                 {
-                    take = 1 + dp[i][j - coins[i]];
+                    take = 1 + curr[j - coins[i]];
                 }
-                dp[i][j] = Math.min(take,notTake);
+                curr[j] = Math.min(take,notTake);
             }
+            prev = curr;
         }
-        if(dp[n - 1][amount] >= 1e9)
+
+        if(prev[amount] >= 1e9)
         {
             return -1;
         }
-        return dp[n - 1][amount];
+        return prev[amount];
+
+        // Tabulation
+
+        // for(int j = 0;j <= amount;j++)
+        // {
+        //     if(j % coins[0] == 0)
+        //     {
+        //         dp[0][j] = j / coins[0];
+        //     }
+        //     else
+        //     {
+        //         dp[0][j] = (int)1e9;
+        //     }
+        // }
+
+        // for(int i = 1;i < n;i++)
+        // {
+        //     for(int j = 0;j <= amount;j++)
+        //     {
+        //         int notTake = dp[i - 1][j];
+        //         int take = (int)1e9;
+        //         if(coins[i] <= j)
+        //         {
+        //             take = 1 + dp[i][j - coins[i]];
+        //         }
+        //         dp[i][j] = Math.min(take,notTake);
+        //     }
+        // }
+        // if(dp[n - 1][amount] >= 1e9)
+        // {
+        //     return -1;
+        // }
+        // return dp[n - 1][amount];
 
         // Memoization
 
