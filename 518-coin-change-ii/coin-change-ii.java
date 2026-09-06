15ccf12e20d09f46c30231f0e169feff63cab854
Sourish Keshav <98088108+sourish02keshav@users.noreply.github.com>
2026-09-06 20:42:52 +0530

    Time: 11 ms (61.62%) | Memory: 57.2 MB (28.95%) - LeetSync

--- 518-coin-change-ii/coin-change-ii.java
+++ 518-coin-change-ii/coin-change-ii.java
@@ -3,17 +3,41 @@ class Solution {
         int n = coins.length;
         int[][] dp = new int[n][amount + 1];
 
-        // Memoization
+        // Tabulation
 
-        for(int i = 0;i < n;i++)
+        for(int j = 0;j <= amount;j++)
+        {
+            if(j % coins[0] == 0)
+            {
+                dp[0][j] = 1;
+            }
+        }
+        for(int idx = 1;idx < n;idx++)
         {
             for(int j = 0;j <= amount;j++)
             {
-                dp[i][j] = -1;
+                int notTake = dp[idx - 1][j];
+                int take = 0;
+                if(coins[idx] <= j)
+                {
+                    take = dp[idx][j - coins[idx]];
+                }
+                dp[idx][j] = take + notTake;
             }
         }
+        return dp[n - 1][amount];
 
-        return fMemoi(n - 1,amount,coins,dp);
+        // Memoization
+
+        // for(int i = 0;i < n;i++)
+        // {
+        //     for(int j = 0;j <= amount;j++)
+        //     {
+        //         dp[i][j] = -1;
+        //     }
+        // }
+
+        // return fMemoi(n - 1,amount,coins,dp);
 
         // Recursion
         // return f(n - 1,amount,coins);
