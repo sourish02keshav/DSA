a562e2c124d376e7b96196fd4ee0aa9f7a64865c
Sourish Keshav <98088108+sourish02keshav@users.noreply.github.com>
2026-09-06 20:40:08 +0530

    Time: 18 ms (27.13%) | Memory: 57.2 MB (22.58%) - LeetSync

---
+++ 518-coin-change-ii/coin-change-ii.java
@@ -0,0 +1,69 @@
+class Solution {
+    public int change(int amount, int[] coins) {
+        int n = coins.length;
+        int[][] dp = new int[n][amount + 1];
+
+        // Memoization
+
+        for(int i = 0;i < n;i++)
+        {
+            for(int j = 0;j <= amount;j++)
+            {
+                dp[i][j] = -1;
+            }
+        }
+
+        return fMemoi(n - 1,amount,coins,dp);
+
+        // Recursion
+        // return f(n - 1,amount,coins);
+    }
+
+    public int fMemoi(int idx,int amount,int[] coins,int[][] dp)
+    {
+        if(idx == 0)
+        {
+            if(amount % coins[idx] == 0)
+            {
+                return 1;
+            }
+            else
+            {
+                return 0;
+            }
+        }
+        if(dp[idx][amount] != -1)
+        {
+            return dp[idx][amount];
+        }
+        int notTake = fMemoi(idx - 1,amount,coins,dp);
+        int take = 0;
+        if(coins[idx] <= amount)
+        {
+            take = fMemoi(idx,amount - coins[idx],coins,dp);
+        }
+        return dp[idx][amount] = take + notTake;
+    }
+
+    public int f(int idx,int amount,int[] coins)
+    {
+        if(idx == 0)
+        {
+            if(amount % coins[idx] == 0)
+            {
+                return 1;
+            }
+            else
+            {
+                return 0;
+            }
+        }
+        int notTake = f(idx - 1,amount,coins);
+        int take = 0;
+        if(coins[idx] <= amount)
+        {
+            take = f(idx,amount - coins[idx],coins);
+        }
+        return take + notTake;
+    }
+}
