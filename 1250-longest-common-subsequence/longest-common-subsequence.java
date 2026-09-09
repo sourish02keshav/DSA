b72559fd9ffffe2f17cca19647d3bc05b616fffe
Sourish Keshav <98088108+sourish02keshav@users.noreply.github.com>
2026-09-10 00:12:56 +0530

    Time: 25 ms (29.20%) | Memory: 54.3 MB (10.44%) - LeetSync

--- 1250-longest-common-subsequence/longest-common-subsequence.java
+++ 1250-longest-common-subsequence/longest-common-subsequence.java
@@ -1,18 +1,5 @@
 class Solution {
 
-    public int f(int idx1,int idx2,String text1,String text2)
-    {
-        if(idx1 == 0 || idx2 == 0)
-        {
-            return 0;
-        }
-        if(text1.charAt(idx1 - 1) == text2.charAt(idx2 - 1))
-        {
-            return 1 + f(idx1 - 1,idx2 - 1,text1,text2);
-        }
-        return Math.max(f(idx1 - 1,idx2,text1,text2),f(idx1,idx2 - 1,text1,text2));
-    }
-
     public int fMemoi(int idx1,int idx2,String text1,String text2,int[][] dp)
     {
         if(idx1 == 0 || idx2 == 0)
@@ -27,14 +14,30 @@ class Solution {
         {
             return dp[idx1][idx2] = 1 + fMemoi(idx1 - 1,idx2 - 1,text1,text2,dp);
         }
-        return dp[idx1][idx2] = Math.max(fMemoi(idx1 - 1,idx2,text1,text2,dp),fMemoi(idx1,idx2 - 1,text1,text2,dp));
+        return dp[idx1][idx2] = Math.max(fMemoi(idx1 - 1,idx2,text1,text2,dp), fMemoi(idx1,idx2 - 1,text1,text2,dp));
+    }
+
+    public int f(int idx1,int idx2,String text1,String text2)
+    {
+        if(idx1 == 0 || idx2 == 0)
+        {
+            return 0;
+        }
+        if(text1.charAt(idx1 - 1) == text2.charAt(idx2 - 1))
+        {
+            return 1 + f(idx1 - 1,idx2 - 1,text1,text2);
+        }
+        return Math.max(f(idx1 - 1,idx2,text1,text2),f(idx1,idx2 - 1,text1,text2));
     }
 
     public int longestCommonSubsequence(String text1, String text2) {
+        
         int m = text1.length();
         int n = text2.length();
-
         int[][] dp = new int[m + 1][n + 1];
+
+        // Memoization
+
         for(int i = 0;i <= m;i++)
         {
             for(int j = 0;j <= n;j++)
