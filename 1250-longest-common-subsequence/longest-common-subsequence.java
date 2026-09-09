205059678dba1b5889579714e81534fea584eac1
Sourish Keshav <98088108+sourish02keshav@users.noreply.github.com>
2026-09-10 00:20:33 +0530

    Time: 19 ms (91.71%) | Memory: 53.7 MB (86.18%) - LeetSync

--- 1250-longest-common-subsequence/longest-common-subsequence.java
+++ 1250-longest-common-subsequence/longest-common-subsequence.java
@@ -36,29 +36,56 @@ class Solution {
         int n = text2.length();
         int[][] dp = new int[m + 1][n + 1];
 
-        for(int i = 0;i <= m;i++)
+        // Space optimization
+
+        int[] prev = new int[n + 1];
+        for(int i = 0;i <= n;i++)
         {
-            dp[i][0] = 0;
-        }
-        for(int j = 0;j <= n;j++)
-        {
-            dp[0][j] = 0;
+            prev[i] = 0;
         }
         for(int i = 1;i <= m;i++)
         {
+            int[] curr = new int[n + 1];
             for(int j = 1;j <= n;j++)
             {
                 if(text1.charAt(i - 1) == text2.charAt(j - 1))
                 {
-                    dp[i][j] = 1 + dp[i - 1][j - 1];
+                    curr[j] = 1 + prev[j - 1];
                 }
                 else
                 {
-                    dp[i][j] = Math.max(dp[i - 1][j],dp[i][j - 1]);
+                    curr[j] = Math.max(prev[j],curr[j - 1]);
                 }
             }
+            prev = curr;
         }
-        return dp[m][n];
+        return prev[n];
+
+        // Tabulation
+
+        // for(int i = 0;i <= m;i++)
+        // {
+        //     dp[i][0] = 0;
+        // }
+        // for(int j = 0;j <= n;j++)
+        // {
+        //     dp[0][j] = 0;
+        // }
+        // for(int i = 1;i <= m;i++)
+        // {
+        //     for(int j = 1;j <= n;j++)
+        //     {
+        //         if(text1.charAt(i - 1) == text2.charAt(j - 1))
+        //         {
+        //             dp[i][j] = 1 + dp[i - 1][j - 1];
+        //         }
+        //         else
+        //         {
+        //             dp[i][j] = Math.max(dp[i - 1][j],dp[i][j - 1]);
+        //         }
+        //     }
+        // }
+        // return dp[m][n];
 
         // Memoization
 
