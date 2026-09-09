56bb7b0588c89fd5d0d3fa7bde41dad7d9568d34
Sourish Keshav <98088108+sourish02keshav@users.noreply.github.com>
2026-09-10 00:16:26 +0530

    Time: 20 ms (78.29%) | Memory: 53.9 MB (68.86%) - LeetSync

--- 1250-longest-common-subsequence/longest-common-subsequence.java
+++ 1250-longest-common-subsequence/longest-common-subsequence.java
@@ -36,16 +36,40 @@ class Solution {
         int n = text2.length();
         int[][] dp = new int[m + 1][n + 1];
 
-        // Memoization
-
         for(int i = 0;i <= m;i++)
         {
-            for(int j = 0;j <= n;j++)
+            dp[i][0] = 0;
+        }
+        for(int j = 0;j <= n;j++)
+        {
+            dp[0][j] = 0;
+        }
+        for(int i = 1;i <= m;i++)
+        {
+            for(int j = 1;j <= n;j++)
             {
-                dp[i][j] = -1;
+                if(text1.charAt(i - 1) == text2.charAt(j - 1))
+                {
+                    dp[i][j] = 1 + dp[i - 1][j - 1];
+                }
+                else
+                {
+                    dp[i][j] = Math.max(dp[i - 1][j],dp[i][j - 1]);
+                }
             }
         }
-        return fMemoi(m,n,text1,text2,dp);
+        return dp[m][n];
+
+        // Memoization
+
+        // for(int i = 0;i <= m;i++)
+        // {
+        //     for(int j = 0;j <= n;j++)
+        //     {
+        //         dp[i][j] = -1;
+        //     }
+        // }
+        // return fMemoi(m,n,text1,text2,dp);
 
         // Recursion
         // return f(m,n,text1,text2);
