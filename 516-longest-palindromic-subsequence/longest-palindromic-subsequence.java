85b9878d05a73880647a16c7b31fd43e874f44a9
Sourish Keshav <98088108+sourish02keshav@users.noreply.github.com>
2026-09-10 15:24:46 +0530

    Time: 59 ms (28.18%) | Memory: 54.9 MB (89.39%) - LeetSync

--- 516-longest-palindromic-subsequence/longest-palindromic-subsequence.java
+++ 516-longest-palindromic-subsequence/longest-palindromic-subsequence.java
@@ -8,16 +8,34 @@ class Solution {
         }
         int[][] dp = new int[m + 1][m + 1];
 
-        // Memoization
+        // Tabulation
 
-        for(int i = 0;i <= m ;i++)
+        for(int i = 1;i <= m;i++)
         {
-            for(int j = 0;j <= m;j++)
+            for(int j = 1;j <= m;j++)
             {
-                dp[i][j] = -1;
+                if(s.charAt(i - 1) == rev.charAt(j - 1))
+                {
+                    dp[i][j] = 1 + dp[i - 1][j - 1];
+                }
+                else
+                {
+                    dp[i][j] = Math.max(dp[i - 1][j],dp[i][j - 1]);
+                }
             }
         }
-        return fMemoi(m,m,s,rev,dp);
+        return dp[m][m];
+
+        // Memoization
+
+        // for(int i = 0;i <= m ;i++)
+        // {
+        //     for(int j = 0;j <= m;j++)
+        //     {
+        //         dp[i][j] = -1;
+        //     }
+        // }
+        // return fMemoi(m,m,s,rev,dp);
 
         // Recursion
         //return f(m,m,s,rev);
