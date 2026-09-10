0f0328798a86d334f2d59a8a23300afd6fec370a
Sourish Keshav <98088108+sourish02keshav@users.noreply.github.com>
2026-09-10 22:12:23 +0530

    Time: 7 ms (69.75%) | Memory: 46.7 MB (15.19%) - LeetSync

--- 583-delete-operation-for-two-strings/delete-operation-for-two-strings.java
+++ 583-delete-operation-for-two-strings/delete-operation-for-two-strings.java
@@ -5,21 +5,46 @@ class Solution {
         int n = word2.length();
         int[][] dp = new int[m + 1][n + 1];
 
+        // Space optimization
+
+        int[] prev = new int[n + 1];
+
         for(int i = 1;i <= m;i++)
         {
+            int[] curr = new int[n + 1];
             for(int j = 1;j <= n;j++)
             {
                 if(word1.charAt(i - 1) == word2.charAt(j - 1))
                 {
-                    dp[i][j] = 1 + dp[i - 1][j - 1];
+                    curr[j] = 1 + prev[j - 1];
                 }
                 else
                 {
-                    dp[i][j] = Math.max(dp[i - 1][j],dp[i][j -1]);
+                    curr[j] = Math.max(prev[j],curr[j - 1]);
                 }
             }
+            prev = curr;
         }
-        int lcs = dp[m][n];
+
+        int lcs = prev[n];
+
+        // Tabulation
+
+        // for(int i = 1;i <= m;i++)
+        // {
+        //     for(int j = 1;j <= n;j++)
+        //     {
+        //         if(word1.charAt(i - 1) == word2.charAt(j - 1))
+        //         {
+        //             dp[i][j] = 1 + dp[i - 1][j - 1];
+        //         }
+        //         else
+        //         {
+        //             dp[i][j] = Math.max(dp[i - 1][j],dp[i][j -1]);
+        //         }
+        //     }
+        // }
+        // int lcs = dp[m][n];
 
         // Memoization
 
