acd9dbedb853801c849e1391a480a9bea0ee6af8
Sourish Keshav <98088108+sourish02keshav@users.noreply.github.com>
2026-09-18 16:07:07 +0530

    Time: 3 ms (98.09%) | Memory: 47.4 MB (17.98%) - LeetSync

---
+++ 72-edit-distance/edit-distance.java
@@ -0,0 +1,59 @@
+class Solution {
+    public int minDistance(String word1, String word2) {
+        int m = word1.length();
+        int n = word2.length();
+        int[][] dp = new int[m + 1][n + 1];
+
+        for(int i = 0;i <= m;i++)
+        {
+            for(int j = 0;j <= n;j++)
+            {
+                dp[i][j] = -1;
+            }
+        }
+        return fMemoi(m,n,word1,word2,dp);
+
+        // Recursion - TC - O(exponential) and SC - O(m + n)
+        // return f(m,n,word1,word2);
+    }
+
+    public int fMemoi(int i,int j,String str1,String str2,int[][] dp)
+    {
+        if(i == 0)
+        {
+            return j;
+        }
+        if(j == 0)
+        {
+            return i;
+        }
+        if(dp[i][j] != -1)
+        {
+            return dp[i][j];
+        }
+        if(str1.charAt(i - 1) == str2.charAt(j - 1))
+        {
+            return dp[i][j] = fMemoi(i - 1,j - 1,str1,str2,dp);
+        }
+        return dp[i][j] = 1 + Math.min(fMemoi(i - 1,j,str1,str2,dp),
+        Math.min(fMemoi(i,j - 1,str1,str2,dp),fMemoi(i - 1,j - 1,str1,str2,dp)));
+    }
+
+    public int f(int i,int j,String str1,String str2)
+    {
+        if(i == 0)
+        {
+            return j;
+        }
+        if(j == 0)
+        {
+            return i;
+        }
+        if(str1.charAt(i - 1) == str2.charAt(j - 1))
+        {
+            return f(i - 1,j - 1,str1,str2);
+        }
+        return 1 + Math.min(f(i - 1,j,str1,str2),Math.min(f(i,j - 1,str1,str2),
+        f(i - 1,j - 1,str1,str2)));
+    }
+}
