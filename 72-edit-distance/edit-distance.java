47b4a4d954a5ef44d860452a6a3a8095ab6a6471
Sourish Keshav <98088108+sourish02keshav@users.noreply.github.com>
2026-09-18 16:10:56 +0530

    Time: 5 ms (65.82%) | Memory: 47.5 MB (6.09%) - LeetSync

--- 72-edit-distance/edit-distance.java
+++ 72-edit-distance/edit-distance.java
@@ -6,12 +6,40 @@ class Solution {
 
         for(int i = 0;i <= m;i++)
         {
-            for(int j = 0;j <= n;j++)
+            dp[i][0] = i;
+        }
+        for(int j = 0;j <= n;j++)
+        {
+            dp[0][j] = j;
+        }
+
+        for(int i = 1;i <= m;i++)
+        {
+            for(int j = 1;j <= n;j++)
             {
-                dp[i][j] = -1;
+                if(word1.charAt(i - 1) == word2.charAt(j - 1))
+                {
+                    dp[i][j] = dp[i - 1][j - 1];
+                }
+                else
+                {
+                    dp[i][j] = 1 + Math.min(dp[i - 1][j],
+                    Math.min(dp[i][j - 1],dp[i - 1][j - 1]));
+                }
             }
         }
-        return fMemoi(m,n,word1,word2,dp);
+
+        return dp[m][n];
+
+        //Memoziation - TC - O(m * n) and SC - O(m * n) and O(m + n)
+        // for(int i = 0;i <= m;i++)
+        // {
+        //     for(int j = 0;j <= n;j++)
+        //     {
+        //         dp[i][j] = -1;
+        //     }
+        // }
+        // return fMemoi(m,n,word1,word2,dp);
 
         // Recursion - TC - O(exponential) and SC - O(m + n)
         // return f(m,n,word1,word2);
