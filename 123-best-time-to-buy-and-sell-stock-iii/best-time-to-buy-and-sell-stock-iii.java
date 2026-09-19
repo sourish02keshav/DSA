cbd79a3cf237243251bd8d30f3bf7a8292e776c1
Sourish Keshav <98088108+sourish02keshav@users.noreply.github.com>
2026-09-19 13:27:11 +0530

    Time: 177 ms (5.10%) | Memory: 137.2 MB (11.12%) - LeetSync

---
+++ 123-best-time-to-buy-and-sell-stock-iii/best-time-to-buy-and-sell-stock-iii.java
@@ -0,0 +1,64 @@
+class Solution {
+    public int maxProfit(int[] prices) {
+        int n = prices.length;
+        int[][][] dp = new int[n + 1][2][3];
+
+        for(int i = 0;i <= n;i++)
+        {
+            for(int j = 0;j <= 1;j++)
+            {
+                for(int k = 0;k <= 2;k++)
+                {
+                    dp[i][j][k] = -1;
+                }
+            }
+        }
+        return fMemoi(0,1,2,n,prices,dp);
+
+        // Recursion - O(exponential) and SC - O(n)
+        // return f(0,1,2,n,prices);
+    }
+
+    public int fMemoi(int idx,int buy,int cap,int n,int[] prices,int[][][] dp)
+    {
+        if(idx == n || cap == 0)
+        {
+            return 0;
+        }
+        if(dp[idx][buy][cap] != -1)
+        {
+            return dp[idx][buy][cap];
+        }
+        if(buy == 1)
+        {
+            dp[idx][buy][cap] = Math.max(-prices[idx] + fMemoi(idx + 1,0,cap,n,prices,dp),
+            fMemoi(idx + 1,buy,cap,n,prices,dp));
+        }
+        else
+        {
+            dp[idx][buy][cap] = Math.max(prices[idx] + fMemoi(idx + 1,1,cap - 1,n,prices,dp),
+            fMemoi(idx + 1,buy,cap,n,prices,dp));
+        }
+        return dp[idx][buy][cap];
+    }
+
+    public int f(int idx,int buy,int cap,int n,int[] prices)
+    {
+        if(idx == n || cap == 0)
+        {
+            return 0;
+        }
+        int profit = 0;
+        if(buy == 1)
+        {
+            profit = Math.max(-prices[idx] + f(idx + 1,0,cap,n,prices),
+            f(idx + 1,buy,cap,n,prices));
+        }
+        else
+        {
+            profit = Math.max(prices[idx] + f(idx + 1,1,cap - 1,n,prices),
+            f(idx + 1,buy,cap,n,prices));
+        }
+        return profit;
+    }
+}
