8f927624516af9faf02cbcc0f8369868116927fc
Sourish Keshav <98088108+sourish02keshav@users.noreply.github.com>
2026-09-19 13:41:35 +0530

    Time: 54 ms (39.18%) | Memory: 118.5 MB (27.59%) - LeetSync

--- 123-best-time-to-buy-and-sell-stock-iii/best-time-to-buy-and-sell-stock-iii.java
+++ 123-best-time-to-buy-and-sell-stock-iii/best-time-to-buy-and-sell-stock-iii.java
@@ -3,6 +3,9 @@ class Solution {
         int n = prices.length;
         int[][][] dp = new int[n + 1][2][3];
 
+        // Space Optimization
+        int[][] ahead = new int[2][3];
+        int[][] curr = new int[2][3];
         for(int idx = n - 1;idx >= 0;idx--)
         {
             for(int buy = 0;buy <= 1;buy++)
@@ -11,18 +14,41 @@ class Solution {
                 {
                     if(buy == 1)
                     {
-                        dp[idx][buy][cap] = Math.max(-prices[idx] + dp[idx + 1][0][cap],
-                        dp[idx + 1][buy][cap]);
+                        curr[buy][cap] = Math.max(-prices[idx] + ahead[0][cap],
+                        ahead[buy][cap]);
                     }
                     else
                     {
-                        dp[idx][buy][cap] = Math.max(prices[idx] + dp[idx + 1][1][cap - 1],
-                        dp[idx+ 1][buy][cap]);
+                        curr[buy][cap] = Math.max(prices[idx] + ahead[1][cap - 1],
+                        ahead[buy][cap]);
                     }
                 }
             }
+            ahead = curr;
         }
-        return dp[0][1][2];
+        return ahead[1][2];
+
+        // Tabulation - TC - O(n * 2 * 3) and SC - O(n * 2 * 3)
+        // for(int idx = n - 1;idx >= 0;idx--)
+        // {
+        //     for(int buy = 0;buy <= 1;buy++)
+        //     {
+        //         for(int cap = 1;cap <= 2;cap++)
+        //         {
+        //             if(buy == 1)
+        //             {
+        //                 dp[idx][buy][cap] = Math.max(-prices[idx] + dp[idx + 1][0][cap],
+        //                 dp[idx + 1][buy][cap]);
+        //             }
+        //             else
+        //             {
+        //                 dp[idx][buy][cap] = Math.max(prices[idx] + dp[idx + 1][1][cap - 1],
+        //                 dp[idx+ 1][buy][cap]);
+        //             }
+        //         }
+        //     }
+        // }
+        // return dp[0][1][2];
         
         // Memoization - TC - O(n * 2 * 3) and SC - O(n * 2 * 3) and O(n)
         // for(int i = 0;i <= n;i++)
