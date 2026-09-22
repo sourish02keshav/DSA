99f7892c3a9818506872cc2ffa99a526538c4ed8
Sourish Keshav <98088108+sourish02keshav@users.noreply.github.com>
2026-09-23 00:12:10 +0530

    Time: 415 ms (5.90%) | Memory: 236.8 MB (22.41%) - LeetSync

---
+++ 300-longest-increasing-subsequence/longest-increasing-subsequence.java
@@ -0,0 +1,51 @@
+class Solution {
+    public int lengthOfLIS(int[] nums) {
+        int n = nums.length;
+        int[][] dp = new int[n][n + 1];
+        // Memoization
+        for(int i = 0;i < n;i++)
+        {
+            for(int j = 0;j <= n;j++)
+            {
+                dp[i][j] = -1;
+            }
+        }
+        return fMemoi(0,-1,n,nums,dp);
+
+        // Recursion
+        // return f(0,-1,n,nums);
+    }
+
+    public int fMemoi(int idx,int prev_idx,int n,int[] arr,int[][] dp)
+    {
+        if(idx == n)
+        {
+            return 0;
+        }
+        if(dp[idx][prev_idx + 1] != -1)
+        {
+            return dp[idx][prev_idx + 1];
+        }
+        int len = fMemoi(idx + 1,prev_idx,n,arr,dp);
+        if(prev_idx == -1 || arr[idx] > arr[prev_idx])
+        {
+            len = Math.max(len,1 + fMemoi(idx + 1,idx,n,arr,dp));
+        }
+        return dp[idx][prev_idx + 1] = len;
+    }
+
+
+    public int f(int idx,int prev_idx,int n,int[] arr)
+    {
+        if(idx == n)
+        {
+            return 0;
+        }
+        int len = f(idx + 1,prev_idx,n,arr);
+        if(prev_idx == -1 || arr[idx] > arr[prev_idx])
+        {
+            len = Math.max(len,1 + f(idx + 1,idx,n,arr));
+        }
+        return len;
+    }
+}
